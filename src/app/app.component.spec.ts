177ed814ef857c2558e6599e436b9155e070a68e
Jessica C <jcake2@gmail.com>
2018-12-10 21:34:26 -0600

    Completes Day 6

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,6 +5,7 @@ import { Day2Component } from './day2/day2.component';
 import { Day3Component } from './day3/day3.component';
 import { Day4Component } from './day4/day4.component';
 import { Day5Component } from './day5/day5.component';
+import { Day6Component } from './day6/day6.component';
 import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
@@ -12,7 +13,8 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        Day1Component, Day2Component, Day3Component, Day4Component, Day5Component
+        Day1Component, Day2Component, Day3Component, Day4Component, Day5Component,
+        Day6Component
       ],
       imports: [FormsModule]
     }).compileComponents();
--- src/app/day6/day6.component.ts
+++ src/app/day6/day6.component.ts
@@ -16,9 +16,124 @@ export class Day6Component implements OnInit {
   }
 
   findSize(input: string): number {
-    throw new Error("Method not implemented.");
+    const numbers = input.match(/(\d+)/g);
+    const coords = [];
+    for(let i = 0; i < numbers.length; i += 2){
+      coords.push([parseInt(numbers[i]), parseInt(numbers[i+1])])
+    }
+    console.log(coords);
+
+    let minX = 9000;
+    let maxX = 0;
+    let minY = 9000;
+    let maxY = 0;
+
+    coords.forEach((coord, index) => {
+      if(coord[0] > maxX){
+        maxX = coord[0];
+      }
+      if(coord[0] < minX){
+        minX = coord[0];
+      }
+      if(coord[1] > maxY){
+        maxY = coord[1];
+      }
+      if(coord[1] < minY){
+        minY = coord[1];
+      }
+    });
+    console.log(`x range: ${minX} to ${maxX}; y range: ${minY} to ${maxY}`);
+
+    const distanceArray = [];
+    const infiniteIndices = new Set();
+
+    for(let x = 0; x <= maxX * 2; x++){
+      const innerArray = [];
+      for(let y = 0; y <= maxY * 2; y++){
+        const closestIndex = this.closestIndex(coords, x, y);
+        innerArray.push(closestIndex);
+        if(x === 0 || x === maxX * 2 || y === 0 || y === maxY * 2){
+          infiniteIndices.add(closestIndex);
+        }
+      }
+      distanceArray.push(innerArray);
+    }
+    console.log(distanceArray);
+
+
+    const finiteIndices = [];
+    coords.forEach((coord, index) => {
+      if(!infiniteIndices.has(index)){
+        finiteIndices.push(index);
+      }
+    });
+
+    console.log(finiteIndices);
+
+    let largestArea = 0;
+    finiteIndices.forEach((index) => {
+      let count = 0;
+      distanceArray.forEach((innerArray) => {
+        innerArray.forEach((value) => {
+          if(value === index){
+            count++;
+          }
+        });
+      });
+      if(count > largestArea){
+        largestArea = count;
+      }
+
+    });
+
+    return largestArea;
   }
 
-  // TODO Remove outliers (max/min x and y coords) and then calculate areas of remaining points
+  closestIndex(coords, x, y){
+    let closestDistance = 999999;
+    let closestIndex = -1;
+    coords.forEach((coord, index) => {
+      const distance = Math.abs(coord[0] - x) + Math.abs(coord[1] - y);
+      if(distance < closestDistance){
+        closestDistance = distance;
+        closestIndex = index;
+      }
+      else if(distance === closestDistance){
+        closestIndex = -1;
+        return;
+      }
+    });
+    return closestIndex;
+  }
+
+  findSizeTotalDistance(input: string): number {
+    const numbers = input.match(/(\d+)/g);
+    const coords = [];
+    for(let i = 0; i < numbers.length; i += 2){
+      coords.push([parseInt(numbers[i]), parseInt(numbers[i+1])])
+    }
+    console.log(coords);
+
+    let within10000Count = 0;
+
+    for(let x = 0; x <= 9000; x++){
+      for(let y = 0; y <= 9000; y++){
+        if(this.distancesSum(coords, x, y) < 10000){
+          within10000Count++;
+        }
+      }
+      
+    }
+    return within10000Count;;
+  }
+
+  distancesSum(coords, x, y){
+    let distancesSum = 0;
+    coords.forEach((coord, index) => {
+      const distance = Math.abs(coord[0] - x) + Math.abs(coord[1] - y);
+      distancesSum += distance;
+    });
+    return distancesSum;
+  }
 
 }
