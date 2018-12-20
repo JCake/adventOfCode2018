f678bea26e97777facf18e147638f47bccec5edc
Jessica C <jcake2@gmail.com>
2018-12-19 23:03:00 -0600

    Day 8 part 2

--- src/app/day8/day8.component.spec.ts
+++ src/app/day8/day8.component.spec.ts
@@ -38,17 +38,61 @@ describe('Day8Component', () => {
       expect(component.sumMetadata('3 2 3 1 0 1 4 0 1 2 0 1 1 5 3 2 0 1 1 0 1 3 0 1 5 6 3 0 3 1 2 3 9 2'))
         .toEqual(5 + 6 + 3 + 1 + 2 + 3 + 9 + 2 + 4 + 2 + 1 + 1 + 3 + 5);
     });
-
-    // TODO test with > 2 child nodes on a branch
   });
-  /*
-   @Test
-  public void sumForDeeperMultipleNodesMultipleMetadataEntriesEach(){
-    assertEquals(138, memory.sumMetadata("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"));
-  }
+  
+  fdescribe('find root node value', () => {
+    it('should find root node value for single node with single metadata entry', () => {
+      expect(component.rootValue('0 1 5')).toEqual(5);
+    });
 
-  // TODO Next test - more nesting (more sub nodes that have subnodes)
-}
+    it('should find root node value for single node with multiple metadata entries', () => {
+      expect(component.rootValue('0 2 5 7')).toEqual(12);
+    });
 
-  */
+    it('should find root node value for one parent and one child nodes with single metadata each and parent references child', () => {
+      expect(component.rootValue('1 1 0 1 6 1')).toEqual(6);
+    });
+
+    it('should find root node value for one parent and one child nodes with single metadata each and parent does not reference child', () => {
+      expect(component.rootValue('1 1 0 1 6 2')).toEqual(0);
+    });
+
+    it('should find root node value for one parent and two child nodes with second one referenced by metadata', () => {
+      expect(component.rootValue('2 1 0 1 6 0 1 3 2')).toEqual(3);
+    });
+
+    it('should find root node value for one parent and two child nodes with first one referenced by metadata', () => {
+      expect(component.rootValue('2 1 0 1 6 0 1 3 1')).toEqual(6);
+    });
+
+    it('should find root node value for one parent and two child nodes with both children referenced by metadata', () => {
+      expect(component.rootValue('2 2 0 1 6 0 1 3 2 1')).toEqual(9);
+    });
+
+    it('should find root node value for one parent and two child nodes with multiple metadata and with both children referenced by metadata', () => {
+      expect(component.rootValue('2 2 0 2 6 4 0 2 3 1 2 1')).toEqual(14);
+    });
+
+    it('should find root node value for one parent and two child nodes some references by metadata out of range', () => {
+      expect(component.rootValue('2 4 0 2 6 4 0 2 3 1 2 1 3 0')).toEqual(14);
+    });
+
+    it('should find root node value for more deeply nested node tree', () => { 
+      expect(component.rootValue('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2')).toEqual(66);
+    });
+
+    it('should find the root value for more branches in node tree', () => {
+      expect(component.rootValue('2 3 1 2 0 3 10 11 12 1 9 1 1 0 1 99 2 1 1 2')).toEqual(66);  
+    });
+
+    // it('should find sum for root with three direct children', () => {
+    //   expect(component.sumMetadata('3 2 0 1 5 0 2 6 3 0 3 1 2 3 9 2')).toEqual(5 + 6 + 3 + 1 + 2 + 3 + 9 + 2);
+    // });
+
+    // it('should find sum for deeper trees with > 2 children of nodes', () => {
+    //   expect(component.sumMetadata('3 2 3 1 0 1 4 0 1 2 0 1 1 5 3 2 0 1 1 0 1 3 0 1 5 6 3 0 3 1 2 3 9 2'))
+    //     .toEqual(5 + 6 + 3 + 1 + 2 + 3 + 9 + 2 + 4 + 2 + 1 + 1 + 3 + 5);
+    // });
+  });
+
 });
--- src/app/day8/day8.component.ts
+++ src/app/day8/day8.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getPreviousOrParentTNode } from '@angular/core/src/render3/state';
 
 @Component({
   selector: 'app-day8',
@@ -6,6 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day8.component.css']
 })
 export class Day8Component implements OnInit {
+  rootValue(input: string): number {
+    const numbers = input.match(/(\d+)/g).map((numStr) => parseInt(numStr));
+    let treeNodes = [];
+    let parent = null;
+    let currentIndex = 0;
+    while(currentIndex < numbers.length){
+      const childNodes = numbers[currentIndex];
+      const metadataCount = numbers[currentIndex + 1];
+      currentIndex += 2;
+      if(childNodes === 0){
+        
+        let sum = 0;
+        for(let i = currentIndex; i < currentIndex + metadataCount; i++){
+          sum += numbers[i];  
+        }
+        const leafNode = {value: sum, parent: parent, childrenCount: 0, children: []};
+
+        currentIndex += metadataCount;
+
+        if(parent){
+          parent.children.push(leafNode);
+        
+          console.log(`after summing metadata, now at index ${currentIndex}`);
+  
+          while(parent != null && parent.children.length == parent.childrenCount){
+            const parentMetadataCount = parent.metadataCount;
+            for(let i = currentIndex; i < currentIndex + parentMetadataCount; i++){
+              console.log(`metadata for parent is at overall index ${i}`);
+              const metadataAsIndex = numbers[i] - 1;
+              console.log(`metadataAsIndex: ${metadataAsIndex}`);
+              if(metadataAsIndex < parent.childrenCount && metadataAsIndex >= 0){
+                // TODO get smarter about how this indexing works
+                parent.value += parent.children[metadataAsIndex].value;
+              }
+            }
+            currentIndex += parentMetadataCount;
+            parent = parent.parent;
+          } 
+        } else {
+          treeNodes.push(leafNode);
+        }
+        
+      } else {
+        const newNode = {childrenCount: childNodes, children: [], parent: parent,
+          metadataCount: metadataCount, metadataValues: [], value: 0};
+        if(parent){
+          parent.children.push(newNode);
+        } else {
+          treeNodes.push(newNode);
+        }
+        parent = newNode;
+        
+        
+      }
+      
+    }
+    return treeNodes[0].value;
+  }
   sumMetadata(input: string): number {
     const numbers = input.match(/(\d+)/g).map((numStr) => parseInt(numStr));
     let treeNodes = []; // TODO or maybe tree node sums?
